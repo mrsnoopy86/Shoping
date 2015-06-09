24a7206ac76e77c3c60fad2c61e0080131a83b4f
mrsnoopy86 <tremtyachiy@gmail.com>
2015-06-09 15:34:20 +0300

    add tab2

--- app/src/main/java/ua/kh/tremtyachiy/shoping/MainScreen.java
+++ app/src/main/java/ua/kh/tremtyachiy/shoping/MainScreen.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TabHost;
 
 /**
  * Created by User on 09.06.2015.
@@ -19,6 +20,41 @@ public class MainScreen extends Activity {
         setContentView(R.layout.main_screen_activity);
 
         initToolbar();
+        initTabs();
+    }
+
+    private void initTabs() {
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec;
+        tabSpec = tabHost.newTabSpec("tag1");
+        tabSpec.setIndicator(getString(R.string.spisok_tab));
+        tabSpec.setContent(R.id.tab1);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag2");
+        tabSpec.setIndicator(getString(R.string.spisok_tab2));
+        tabSpec.setContent(R.id.tab2);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag3");
+        tabSpec.setIndicator(getString(R.string.spisok_tab3));
+        tabSpec.setContent(R.id.tab3);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag4");
+        tabSpec.setIndicator(getString(R.string.spisok_tab4));
+        tabSpec.setContent(R.id.tab4);
+        tabHost.addTab(tabSpec);
+
+        tabHost.setCurrentTabByTag("tag1");
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+
+            }
+        });
     }
 
     private void initToolbar() {
