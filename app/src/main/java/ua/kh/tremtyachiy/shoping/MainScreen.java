b79a3185c1589fb5d7d3829b28b0a7536f792e14
mrsnoopy86 <tremtyachiy@gmail.com>
2015-06-09 13:57:45 +0300

    Create MainScreen

---
+++ app/src/main/java/ua/kh/tremtyachiy/shoping/MainScreen.java
@@ -0,0 +1,16 @@
+package ua.kh.tremtyachiy.shoping;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by User on 09.06.2015.
+ */
+public class MainScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen_activity);
+    }
+}
