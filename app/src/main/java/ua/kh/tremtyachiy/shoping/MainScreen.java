9fc29c64b8da11d264f28cd0a4e8a7d17d6cc2e7
mrsnoopy86 <tremtyachiy@gmail.com>
2015-06-09 14:48:03 +0300

    create toolbar

--- app/src/main/java/ua/kh/tremtyachiy/shoping/MainScreen.java
+++ app/src/main/java/ua/kh/tremtyachiy/shoping/MainScreen.java
@@ -2,15 +2,41 @@ package ua.kh.tremtyachiy.shoping;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 /**
  * Created by User on 09.06.2015.
  */
 public class MainScreen extends Activity {
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen_activity);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+        toolbar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
