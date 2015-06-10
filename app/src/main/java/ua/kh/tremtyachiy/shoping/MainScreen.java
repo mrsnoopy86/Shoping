ca2edb6a1c5ffbdaef3fada1c6f6f43b5d78aed9
mrsnoopy86 <tremtyachiy@gmail.com>
2015-06-10 13:23:07 +0300

    add item menu and image of header

--- app/src/main/java/ua/kh/tremtyachiy/shoping/MainScreen.java
+++ app/src/main/java/ua/kh/tremtyachiy/shoping/MainScreen.java
@@ -1,16 +1,24 @@
 package ua.kh.tremtyachiy.shoping;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.TabHost;
 
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
 import com.mikepenz.materialdrawer.accountswitcher.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 /**
  * Created by User on 09.06.2015.
@@ -33,7 +41,7 @@ public class MainScreen extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if(drawerMenu.isDrawerOpen()){
+        if(drawerMenu != null && drawerMenu.isDrawerOpen()){
             drawerMenu.closeDrawer();
         } else {
             super.onBackPressed();
@@ -41,9 +49,11 @@ public class MainScreen extends AppCompatActivity {
     }
 
     private void initDrawermenu() {
+
         AccountHeader accountHeader = new AccountHeaderBuilder()
                 .withActivity(this)
-                .withHeaderBackground(R.drawable.cover_material)
+                .withHeaderBackground(R.drawable.shopinghelper_drawer_header)
+                .withHeightDp(230)
                 .build();
 
         drawerMenu = new DrawerBuilder()
@@ -52,7 +62,36 @@ public class MainScreen extends AppCompatActivity {
                 .withDisplayBelowToolbar(true)
                 .withActionBarDrawerToggleAnimated(true)
                 .withActivity(this)
-                .build();
+                .addDrawerItems(
+                        new SecondaryDrawerItem()
+                                .withIcon(R.drawable.ic_settings_black_24dp)
+                                .withName(R.string.nav_menu_item1)
+                                .withIdentifier(1),
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.nav_menu_item2)
+                                .withIcon(R.drawable.ic_announcement_black_24dp)
+                                .withIdentifier(2),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.nav_menu_item3)
+                                .withIcon(R.drawable.ic_supervisor_account_black_24dp)
+                                .withIdentifier(3),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.nav_menu_item4)
+                                .withIcon(R.drawable.ic_live_help_black_24dp)
+                                .withIdentifier(4),
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.nav_menu_item5)
+                                .withIcon(R.drawable.ic_exit_to_app_black_24dp)
+                                .withIdentifier(5))
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem iDrawerItem) {
+                        return true;
+                    }
+                })
+                                .build();
     }
 
     private void initTabs() {
