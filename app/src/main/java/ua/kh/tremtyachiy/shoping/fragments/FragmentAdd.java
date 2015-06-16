72cadb21bd1330f126abf9d8e37cef9cdba60364
mrsnoopy86 <tremtyachiy@gmail.com>
2015-06-16 13:35:11 +0300

    work

--- app/src/main/java/ua/kh/tremtyachiy/shoping/fragments/FragmentAdd.java
+++ app/src/main/java/ua/kh/tremtyachiy/shoping/fragments/FragmentAdd.java
@@ -1,11 +1,15 @@
 package ua.kh.tremtyachiy.shoping.fragments;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import ua.kh.tremtyachiy.shoping.R;
 
@@ -14,17 +18,13 @@ import ua.kh.tremtyachiy.shoping.R;
  */
 public class FragmentAdd extends Fragment{
     private View fragmentView;
+    TextView tvDate;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         fragmentView = inflater.inflate(R.layout.fragmentadd, container, false);
+        tvDate =(TextView) fragmentView.findViewById(R.id.textView2);
         return fragmentView;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        
     }
 }
